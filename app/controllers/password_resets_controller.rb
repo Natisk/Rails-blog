e3bbbbe26cc8651fa6a572fcf13d47251a55bd50
vitaly <realnatisk@gmail.com>
2014-05-28 14:53:17 +0300

    admin_user password reset refactor

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,8 +1,10 @@
 class PasswordResetsController < ApplicationController
+
   def new
   end
 
   def create
-    redirect_to root_url, :notice => "Email has been sent to your address"
+    redirect_to root_url, :notice => 'Email has been sent to your address'
   end
+
 end
